89c807eeb16d8901877c05deb8e86d569d349d2c
Leandro <lealmeids@gmail.com>
2021-01-27 20:04:55 -0400

    Realizando o processo de binding

--- app/src/main/java/com/example/aluraviagens/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/com/example/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -2,9 +2,18 @@ package com.example.aluraviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.aluraviagens.R;
+import com.example.aluraviagens.model.Pacote;
+import com.example.aluraviagens.util.DataUtil;
+import com.example.aluraviagens.util.MoedaUtil;
+import com.example.aluraviagens.util.ResourcesUtil;
+
+import java.math.BigDecimal;
 
 public class ResumoCompraActivity extends AppCompatActivity {
 
@@ -12,5 +21,26 @@ public class ResumoCompraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2,
+                new BigDecimal("243.99"));
+
+        setTitle("Resumo da compra");
+
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourcesUtil.devolveDrawable
+                (this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacoteSaoPaulo.getDias());
+        data.setText(dataFormatadaDaViagem);
+
+        TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
